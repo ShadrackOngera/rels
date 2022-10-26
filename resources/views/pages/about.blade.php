d6cc83e4097b0d67f226d09365c9e5c03766d562
shadrackongera <shadrack.sme@gmail.com>
2022-10-26 12:40:05 +0300

    content in about Page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -21,10 +21,12 @@
                             About {{ config('app.name') }}
                         </h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolores eum necessitatibus nisi tempore, voluptas?
+                            Land use is central to addressing sustainability issues, including biodiversity conservation, climate change, food security,
+                            poverty alleviation, and sustainable energy. In this paper, we synthesize knowledge accumulated in land system science,
+                            the integrated study of terrestrial social-ecological systems, into 10 hard truths that have strong, general, empirical support.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, laboriosam nisi quia sed sit soluta vitae. Amet distinctio iste ratione repudiandae sapiente sint tempora vero?
+                            These facts help to explain the challenges of achieving sustainability in land use and thus also point toward solutions.
                         </p>
                     </div>
                     <div class="col-sm-6 mb-3">
@@ -43,13 +45,10 @@
                     <div class="col-sm-6 align-self-center mb-3">
                         <h2>Why we are different</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores earum eveniet fugiat laudantium maxime odit perferendis totam velit veniam.
+                            How human societies use, manage, and interact with land is key to addressing current sustainability issues including nature conservation, climate change, food security, poverty alleviation, and energy transitions, framed in high-level political agreements from the 2030 Agenda for Sustainable Development to the Paris Climate Agreement or the Convention on Biological Diversity.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quidem?
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus eaque ex excepturi expedita in itaque praesentium quia rem!
+                            Despite the centrality of land use to these debates, long-disproven misconceptions, partial framings, and ill-conceived ideas continue to permeate these discussions, such as the misconception that there is abundant land available globally that is “unused” or “uncontested”
                         </p>
                     </div>
                 </div>
@@ -64,10 +63,10 @@
                             More About {{ config('app.name') }}
                         </h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolores eum necessitatibus nisi tempore, voluptas?
+                            Land systems are terrestrial social-ecological systems where human and environmental systems interact through land use and are the focus of the interdisciplinary field of LSS (4, 5). From knowledge accumulated over decades by a large and diverse LSS community, we extracted key insights that scientists, policy and decision makers, and practitioners should understand about land use.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, laboriosam nisi quia sed sit soluta vitae. Amet distinctio iste ratione repudiandae sapiente sint tempora vero?
+                            These insights are akin to “stylized facts” or “empirical regularities” in economics or ecology, i.e., empirical generalizations supported by a solid body of evidence that represent the current state of knowledge on land systems.
                         </p>
                     </div>
                     <div class="col-sm-6 mb-3">
